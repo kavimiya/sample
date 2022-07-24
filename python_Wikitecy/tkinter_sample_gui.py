a11e632dba459a1992f73e3c3bbad0662300ba0d
kavimiya <sathishkavimass@gmail.com>
2022-07-24 19:07:08 +0530

    add

--- python_Wikitecy/tkinter_sample_gui.py
+++ python_Wikitecy/tkinter_sample_gui.py
@@ -1,13 +1,15 @@
 from tkinter import *
-top=tk()
+top=Tk()
 l1=Label(top,text="user name please")
 l1.grid(column=0,row=0)
 
-e1=Entery(top,bd=9)
-e1.grid(olumn=1,row=0)
-e2=Entery(top,bd=9)
-e2.grid(olumn=1,row=0)
+e1=Entry(top,bd=9)
+e1.grid(column=1,row=0)
+e2=Entry(top,bd=9)
+e2.grid(column=1,row=1)
 
 def sub():
-    massagebox.showinfo("your data success")
-    redbut=Button()
+    massagebox.showinfo("your data success",e1.get() + "- your data")
+redbut=Button(top,text="sk Button",fg="red",command=sub)
+redbut.grid(column=2,row=0)
+top.mainloop()
